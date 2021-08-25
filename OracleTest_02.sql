738baab695f6285d9fca5cb523321e4030c7a1f0
노승은 <seun2230@gmail.com>
2021-08-25 23:33:48 +0900

    Oracle 답안지: 다른 방식이용하기

--- OracleTest_02.sql
+++ OracleTest_02.sql
@@ -4,7 +4,7 @@
 -- A11.
 SELECT e.last_name, e.salary, d.department_name
 FROM employees e, departments d
-WHERE d.department_id= e.department_id
+WHERE e.department_id = d.department_id (+)
 AND commission_pct IS NOT NULL;
 
 
@@ -16,7 +16,10 @@ WHERE commission_pct IS NOT NULL;
 -- A12.
 -- last_name이 ‘King’인 사람을 
 -- 매니저로 둔  직원들의 last_name, salary, job_id를 출력해 주십시오.
-
+SELECT emp.last_name, emp.salary, emp.job_id
+FROM employees emp, employees man
+WHERE emp.manager_id = man.employee_id AND
+    man.last_name = 'King';
 SELECT last_name, salary, job_id
 FROM employees
 WHERE manager_id IN (SELECT employee_id FROM employees
@@ -29,6 +32,8 @@ WHERE manager_id IN (SELECT employee_id FROM employees
         WHERE last_name LIKE '%King%');
         
 -- A13. 
+SELECT emp.last_name, emp.salary FROM employees emp, employees man
+WHERE emp.manager_id = man.employee_id AND emp.salary > man.salary;
 --방식 1.
 SELECT e.last_name, salary
 FROM employees e
@@ -47,6 +52,12 @@ SELECT MIN(salary) MIN ,MAX(salary) MAX ,SUM(salary) SUM ,ROUND(AVG(salary)) AVG
 FROM employees;
 
 -- A15.
+SELECT department_id, AVG(salary) FROM employees GROUP BY department_id; -- 서브 쿼리로 사용
+
+SELECT last_name, e.salary 
+FROM employees e, (SELECT department_id, AVG(salary) salary FROM employees GROUP BY department_id) sal
+WHERE e.department_id = sal.department_id AND e.salary < sal.salary;
+
 --방식 1 -> 해결 X
 SELECT e.last_name, e.salary
 FROM employees e
